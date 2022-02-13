b3ea0e983a291dbc10a9663a659f7b0548b4bb56
Kazel <59406000+kazel04@users.noreply.github.com>
2022-02-13 15:56:03 +0800

    New JS for pill menu

--- Javascript/order.js
+++ Javascript/order.js
@@ -51,3 +51,22 @@ $(".msg-close").click(function (e) {
 	e.stopPropagation();
 });
 
+/* JS for tab menu and its contents, referenced from codepen: https://codepen.io/derekjp/pen/pPqwXJ */
+
+$("document").ready(function(){
+    $(".tab-slider--body").hide();
+    $(".tab-slider--body:first").show();
+  });
+  
+  $(".tab-slider--nav li").click(function() {
+    $(".tab-slider--body").hide();
+    var activeTab = $(this).attr("rel");
+    $("#"+activeTab).fadeIn();
+      if($(this).attr("rel") == "tab2"){
+          $('.tab-slider--tabs').addClass('slide');
+      }else{
+          $('.tab-slider--tabs').removeClass('slide');
+      }
+    $(".tab-slider--nav li").removeClass("active");
+    $(this).addClass("active");
+  });
